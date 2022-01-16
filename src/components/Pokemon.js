ddf4c3ecc6b3000b35ae9f4fc6568ba54abcd3cb
“Lynda <lynda.kim.phan@gmail.com>
2022-01-15 23:08:11 -0800

    Successfully made API request

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,36 +1,34 @@
 import React from "react";
 
 export default function Pokemon() {
-  const [char, setChar] = React.useState("");
-  const [pokemon, setPokemon] = React.useState({});
+  let [char, setChar] = React.useState("");
+  let [chosenPokemon, setChosenPokemon] = React.useState(true);
+  let [pokemon, setPokemon] = React.useState({});
+  let [displayData, setDisplayData] = React.useState({ name: "" });
 
-  // when we type in input box, it will save to state
-  function handleChange(event) {
-    setChar((prevChar) => {
-      return {
-        ...prevChar,
-        [event.target.name]: event.target.value,
-      };
-    });
-  }
+  // function getValues() {
+  //   // const charName = pokemon.forms[0].name;
+  //   const charSpecies = pokemon.species;
+  //   const charHeight = pokemon.height;
+  //   setDisplayData([
+  //     {
+  //       // pokemonName: charName,
+  //       species: charSpecies,
+  //       height: charHeight,
+  //     },
+  //   ]);
+  // // }
 
-  function getPokemon() {
-    console.log(pokemon);
-    // setPokemon({
-    //   name: char,
-    // species: data.species.name,
-    // img: data.sprites.front_default,
-    // hp: data.stats[0].base_stat,
-    // attack: data.stats[1].base_stat,
-    // defense: data.stats[2].base_stat,
-    // type: data.types[0].base_stat,
+  function getValues() {
+    const pName = pokemon;
+    setDisplayData({ name: pName });
   }
 
   React.useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${char}`)
       .then((res) => res.json())
-      .then((data) => setPokemon(data.results.name));
-  }, []);
+      .then((data) => setPokemon(data.species.name));
+  }, [char]);
 
   return (
     <main>
@@ -39,20 +37,31 @@ export default function Pokemon() {
           type="text"
           placeholder="Pokemon Name"
           className="form--input"
-          onChange={handleChange}
+          onChange={(e) => setChar(e.target.value)}
         />
-
-        <button className="form--button" onClick={getPokemon}>
-          {" "}
+        <button className="form--button" onClick={getValues}>
           Get Pokemon stats!{" "}
         </button>
-
-        <ul className="pokemonList">
-          {pokemon.map(({ name, url }) => (
-            <li key={url}>(name)</li>
-          ))}
-        </ul>
+      </div>
+      <div className="displayStats">
+        <>
+          {" "}
+          {!chosenPokemon ? (
+            <h1> Please enter a Pokemon name </h1>
+          ) : (
+            <h1 value={displayData.name}>{displayData.name}</h1>
+          )}{" "}
+        </>
       </div>
     </main>
   );
 }
+
+// setPokemon({
+//   name: char,
+// species: data.species.name,
+// img: data.sprites.front_default,
+// hp: data.stats[0].base_stat,
+// attack: data.stats[1].base_stat,
+// defense: data.stats[2].base_stat,
+// type: data.types[0].base_stat,
