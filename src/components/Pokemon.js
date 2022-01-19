dba0b6cbc7893abf346cc08589329dd8f2da8e82
“Lynda <lynda.kim.phan@gmail.com>
2022-01-18 22:02:39 -0800

    made CSS changes

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,28 +3,27 @@ import React from "react";
 export default function Pokemon() {
   let [char, setChar] = React.useState("");
   let [chosenPokemon, setChosenPokemon] = React.useState(true);
-  // let [pokemon, setPokemon] = React.useState({});
-  let [displayData, setDisplayData] = React.useState({
-    name: "",
-    species: "",
-    height: "",
-  });
+  let [displayData, setDisplayData] = React.useState({});
 
   function getValues() {
     fetch(`https://pokeapi.co/api/v2/pokemon/${char}`)
       .then((res) => res.json())
       .then((data) => {
-        // setPokemon(data);
         setDisplayData({
           name: data.forms[0].name,
           species: data.species.name,
           height: data.height,
+          img: data.sprites.front_default,
+          hp: data.stats[0].base_stat,
+          attack: data.stats[1].base_stat,
+          defense: data.stats[2].base_stat,
+          type: data.types[0].type.name,
         });
       });
   }
 
   return (
-    <main>
+    <main className="flexbox-container">
       <div className="form">
         <input
           type="text"
@@ -36,6 +35,7 @@ export default function Pokemon() {
           Get Pokemon stats!{" "}
         </button>
       </div>
+
       <div className="displayStats">
         <>
           {" "}
@@ -43,14 +43,20 @@ export default function Pokemon() {
             <h1> Please enter a Pokemon name </h1>
           ) : (
             <>
-              <h1 value={displayData.name}>
-                {" "}
-                Pokemon Name: {displayData.name}{" "}
-              </h1>
-              <h2 value={displayData.species}>
+              <h1 value={displayData.name}> {displayData.name} </h1>
+              <img src={displayData.img}></img>
+              <p value={displayData.species}>
                 {" "}
                 Species: {displayData.species}{" "}
-              </h2>
+              </p>
+              <p value={displayData.type}> Type: {displayData.type} </p>
+              <p value={displayData.height}> Height: {displayData.height} </p>
+              <p value={displayData.hp}> HP: {displayData.hp} </p>
+              <p value={displayData.attack}> Attack: {displayData.attack} </p>
+              <p value={displayData.defense}>
+                {" "}
+                Defense: {displayData.defense}{" "}
+              </p>
             </>
           )}{" "}
         </>
